f3785ae80e6212a0a034fb447d32f07d6e0b72ac
yns-jns <younessaitmbarek97@gmail.com>
2023-12-15 15:11:48 +0100

    add home page and audio

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 // Scripting function for the whole application
 const mainApp = () => {
 
+    // ----------------------------------------------------------------
+    // Une interaction utilisateur pour déclencher l'autoplay après le chargement de la page
+    // window.onload = function () {
+    //     document.querySelector('audio').play();
+    // };
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const audio = document.getElementById("myAudio");
+        const audioControlButton = document.getElementById("audioControlButton");
+
+        audioControlButton.addEventListener("click", function () {
+            if (audio.paused) {
+                audio.play();
+            } else {
+                audio.pause();
+            }
+        });
+    });
+    // ----------------------------------------------------------------
+
     // Robot Options
     const robotOptions = ["rock", "paper", "scissors"];
     // Global variables
@@ -47,7 +67,7 @@ const mainApp = () => {
                 setTimeout(() => {
                     // Call compare hands function
                     compareHands(this.textContent, robotChoice);
-                    //Update Images
+                    // Update Images
                     playerHand.src = `./images/${this.textContent}.png`;
                     robotHand.src = `./images/${robotChoice}.png`;
                 }, 2000);
@@ -77,6 +97,7 @@ const mainApp = () => {
         const winner = document.querySelector(".winner-zone");
         // Checking for a draw
         if (playerChoice === robotChoice) {
+            winner.style.color = "darkgrey";
             winner.textContent = "Draw!";
             return;
         }
@@ -84,12 +105,14 @@ const mainApp = () => {
         // Checking for a rock
         if (playerChoice === "rock") {
             if (robotChoice === "scissors") {
+                winner.style.color = "green";
                 winner.textContent = "Player Wins ;)";
                 // winner.textContent = `${plaer} Wins ;)`;
                 playerScore++;
                 updateScore();
                 return;
             } else {
+                winner.style.color = "red";
                 winner.textContent = "Robot Wins ;)";
                 robotScore++;
                 updateScore();
@@ -99,11 +122,13 @@ const mainApp = () => {
         //Check for Paper
         if (playerChoice === "paper") {
             if (robotChoice === "scissors") {
+                winner.style.color = "red";
                 winner.textContent = "Robot Wins ;)";
                 robotScore++;
                 updateScore();
                 return;
             } else {
+                winner.style.color = "green";
                 winner.textContent = "Player Wins ;)";
                 playerScore++;
                 updateScore();
@@ -113,11 +138,13 @@ const mainApp = () => {
         //Check for Scissors
         if (playerChoice === "scissors") {
             if (robotChoice === "rock") {
+                winner.style.color = "red";
                 winner.textContent = "Robot Wins ;)";
                 robotScore++;
                 updateScore();
                 return;
             } else {
+                winner.style.color = "green";
                 winner.textContent = "Player Wins ;)";
                 playerScore++;
                 updateScore();
@@ -134,3 +161,24 @@ const mainApp = () => {
 
 // Calling mainApp to start playing
 mainApp();
+
+
+
+// const turnOn = () => {
+//     window.onload = function () {
+//         document.querySelector('audio').play();
+//     };
+// }
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const audio = document.getElementById("myAudio");
+//     const audioControlButton = document.getElementById("audioControlButton");
+
+//     audioControlButton.addEventListener("click", function () {
+//         if (audio.paused) {
+//             audio.play();
+//         } else {
+//             audio.pause();
+//         }
+//     });
+// });
